66ec683ca19e4d0d09453d1bd31064f4ddd6259e
Guilherme Oliveira <46288058+thechibbis@users.noreply.github.com>
2022-11-20 13:59:17 -0300

    Improve userinfo command (#16)
    Improved features
    
    - Moved to e.field instead of e.description
    - Added "cargos" field
    
    
    
    ![20221119_17h25m46s_grim](https://user-images.githubusercontent.com/46288058/202870346-98825a2c-5209-4fdb-b4a4-d0ddee0cdc28.png)

--- src/commands/utils/userinfo.rs
+++ src/commands/utils/userinfo.rs
@@ -11,8 +11,9 @@ pub async fn userinfo(
     let user = user.as_ref().unwrap_or_else(|| cx.author());
     let guild = cx.partial_guild().await.unwrap();
 
-    let user_name = user.tag();
-    let user_id = user.id;
+    let user_name = format!("`{}`", user.name);
+    let user_tag = format!("`{}`", user.tag());
+    let user_id = format!("`{}`", user.id);
 
     let member = guild.member(cx, user.id).await.unwrap();
 
@@ -22,20 +23,29 @@ pub async fn userinfo(
     let account_age_timestamp: i64 = user.created_at().timestamp();
     let account_age = get_discord_relative_time(account_age_timestamp);
 
-    let description = format!(
-        r#"
-            -> **Nome do usuário:**     {user_name} 
-            -> **ID do usuário:**       {user_id}   
-            -> **Entrou no servidor:**  {joined_at}
-            -> **Conta criada: **       {account_age}
-        "#
-    );
+    let avatar = user
+        .avatar_url()
+        .unwrap_or_else(|| user.default_avatar_url());
+
+    let roles = member.roles(cx).unwrap_or_default().into_iter();
+    let mut roles_str = roles.map(|r| r.name).collect::<Vec<_>>().join(", ");
+
+    if roles_str.is_empty() {
+        roles_str = "O usuário não contem nenhum cargo".to_string();
+    } else {
+        roles_str = format!("`{}`", roles_str);
+    }
 
     cx.send(|m| {
         m.embed(|e| {
-            e.title(format!("Informações do usuário: `{user_name}`"))
+            e.title(format!("Informações do usuário: {user_name}"));
+            e.field("🔖 **Tag do discord:**", user_tag, true);
+            e.field("💻 **Id de usuário:**", user_id, true);
+            e.field("📅 **Conta criada há:**", account_age, true);
+            e.field("🌟 **Entrou no servidor há:**", joined_at, false);
+            e.field("📚 **Cargos:**", roles_str, false);
+            e.thumbnail(avatar)
                 .colour(serenity::Colour::DARK_PURPLE)
-                .description(description)
                 .footer(|f| f.text(format!("Comando pedido por {}", cx.author().tag())))
         })
     })

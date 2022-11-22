15ec8b524b7134eb1876e37aeec1ccc08dd629f3
S0raWasTaken <s0ra@duck.com>
2022-11-21 20:28:14 -0500

    chore: clippy
    I also tweaked the code and the command

--- src/commands/utils/userinfo.rs
+++ src/commands/utils/userinfo.rs
@@ -11,20 +11,22 @@ pub async fn userinfo(
     let user = user.as_ref().unwrap_or_else(|| cx.author());
     let guild = cx.partial_guild().await.unwrap();
 
-    let user_name = format!("`{}`", user.name);
     let user_tag = format!("`{}`", user.tag());
     let user_id = format!("`{}`", user.id);
 
     let member = guild.member(cx, user.id).await.unwrap();
 
-    let nick_in_guild = user.nick_in(&serenity::CacheHttp::http(&cx), cx.guild().unwrap().id).await
-    .unwrap_or(user.name.to_string());
+    let nick_in_guild = user
+        .nick_in(&serenity::CacheHttp::http(&cx), cx.guild().unwrap().id)
+        .await
+        .unwrap_or_else(|| user.name.to_string());
 
     let joined_at = member.joined_at.unwrap().timestamp();
 
     let account_age = user.created_at().timestamp();
 
-    let author_avatar = cx.author()
+    let author_avatar = cx
+        .author()
         .avatar_url()
         .unwrap_or_else(|| user.default_avatar_url());
 
@@ -33,40 +35,42 @@ pub async fn userinfo(
         .unwrap_or_else(|| user.default_avatar_url());
 
     let roles = member.roles(cx).unwrap_or_default().into_iter();
-    let mut roles_str = roles.map(|r| r.name).collect::<Vec<_>>().join(", ");
+    let mut roles_str = roles
+        .map(|r| format!("<@&{}>", *r.id.as_u64()))
+        .collect::<Vec<_>>()
+        .join(" ");
 
     if roles_str.is_empty() {
         roles_str = "O usuário não contem nenhum cargo".to_string();
-    } else {
-        roles_str = format!("`{}`", roles_str);
     }
 
     cx.send(|m| {
         m.embed(|e| {
-            e.author(|a: &mut serenity::builder::CreateEmbedAuthor|
-                a.icon_url(member.face())
-                .name(nick_in_guild));
-                e.fields( vec![
-                    ("🔖 **Tag do discord:**", user_tag, true),
-                    ("💻 **Id de usuário:**", user_id, true),
-                    (
-                        "📅 **Conta criada há:**",
-                        get_relative_time(account_age as u64),
-                        true,
-                    ),
-                    (
-                        "🌟 **Entrou no servidor há:**",
-                        get_relative_time(joined_at as u64),
-                        false,
-                    ),
-                    ("📚 **Cargos:**", roles_str, false)
-                ]);
-            e.title(format!("Informações do usuário"));
-            e.thumbnail(avatar)
-                .colour(serenity::Colour::DARK_PURPLE)
-                .footer(|f| 
-                    f.icon_url(author_avatar)
-                    .text(format!("Pedido por {}", cx.author().tag())))
+            e.author(|a: &mut serenity::builder::CreateEmbedAuthor| {
+                a.icon_url(member.face()).name(nick_in_guild)
+            })
+            .title("Informações do usuário")
+            .fields([
+                ("🔖 **Tag do discord:**", user_tag, true),
+                ("💻 **Id de usuário:**", user_id, true),
+                (
+                    "📅 **Conta criada há:**",
+                    get_relative_time(account_age as u64),
+                    true,
+                ),
+                (
+                    "🌟 **Entrou no servidor há:**",
+                    get_relative_time(joined_at as u64),
+                    false,
+                ),
+                ("📚 **Cargos:**", roles_str, false),
+            ])
+            .thumbnail(avatar)
+            .colour(serenity::Colour::DARK_PURPLE)
+            .footer(|f| {
+                f.icon_url(author_avatar)
+                    .text(format!("Pedido por {}", cx.author().tag()))
+            })
         })
     })
     .await?;
